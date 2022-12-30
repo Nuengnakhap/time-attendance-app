c9f39e5406f3b32ff9417c0a9eb3c5d94891cadd
Nuengnakhap <nueng1569@hotmail.com>
2022-12-30 23:51:51 +0700

    update app config

--- app.config.js
+++ app.config.js
@@ -2,7 +2,25 @@ import "dotenv/config";
 
 export default ({ config }) => ({
   ...config,
-  ios: { config: { googleMapsApiKey: process.env.MAP_API_KEY } },
-  android: { config: { googleMaps: { apiKey: process.env.MAP_API_KEY } } },
-  extra: { mapsApiKey: process.env.MAP_API_KEY },
+  ios: {
+    ...config.ios,
+    config: { googleMapsApiKey: process.env.MAP_API_KEY },
+  },
+  android: {
+    ...config.android,
+    config: { googleMaps: { apiKey: process.env.MAP_API_KEY } },
+  },
+  runtimeVersion: {
+    policy: "sdkVersion",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+    url: "https://u.expo.dev/e3cb49a6-14c2-45ed-88eb-9cc6db2d7a0b",
+  },
+  extra: {
+    mapsApiKey: process.env.MAP_API_KEY,
+    eas: {
+      projectId: "e3cb49a6-14c2-45ed-88eb-9cc6db2d7a0b",
+    },
+  },
 });
